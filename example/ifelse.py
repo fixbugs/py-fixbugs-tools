1c0879d93c8fd7898aa6bbbfb702d026ec2cf8a5
fuxin_home <goitt0519@gmail.com>
2016-04-25 22:21:51 +0800

    add if else test

---
+++ example/ifelse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.6
+#-*- coding: utf-8 -*-
+
+age = 20
+if age >= 18:
+    print 'you are 16'
+else:
+    print 'you are 18'
+
+
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+x = 0
+
+if x:
+    print('True')
+
+birth = input('birth: ')
+if birth < 2000:
+    print('00前')
+else:
+    print('00后')
+
