b5ba55c13ec71d254e4fbe7b92ef60eff5956808
fuxin_home <goitt0519@gmail.com>
2016-04-26 22:06:52 +0800

    add sysfunc test example

---
+++ example/sysfunc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.6
+#-*- coding: utf-8 -*-
+
+print abs(-100)
+print abs(-20)
+print abs(12.34)
+
+print max(1, 2)
+print max(2, 3, 1, 5)
+
+print int('123')
+print int(12.34)
+print float('12.34')
+print str(1.23)
+print str(100)
+print bool(1)
+print bool('')
+
+# 为函数取别名
+a = abs
+print a(-1)
+
