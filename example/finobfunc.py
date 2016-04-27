76f8bed879783a0d4a7e41c1ab10627866ee744e
fuxin_home <goitt0519@gmail.com>
2016-04-27 22:04:45 +0800

    add finofunc

---
+++ example/finobfunc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.6
+#-*- coding: utf-8 -*-
+
+
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n - 1)
+
+print fact(1)
+print fact(5)
+print fact(100)
+print fact(1000)
+
+
+#尾递归
+def fact(n):
+    return fact_iter(n, 1)
+
+
+def fact_iter(num, product):
+    if num == 1:
+        return product
+    return fact_iter(num - 1, num * product)
+
+print fact(5)
+print fact(1000)
