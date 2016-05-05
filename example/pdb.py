7faa825288728c993517101707e2970b821c1fef
fuxin_home <goitt0519@gmail.com>
2016-05-05 22:52:15 +0800

    add pdb debug for py

---
+++ example/pdb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.6
+#-*- coding: utf-8 -*-
+
+s = '0'
+n = int(s)
+print(10 / n)
+
+#command python -m pdb pdb.py
+
+
+#test for pdb stop
+# import pdb
+
+# s = '0'
+# n = int(s)
+# pdb.set_trace() # 运行到这里会自动暂停
+# print(10 / n)
