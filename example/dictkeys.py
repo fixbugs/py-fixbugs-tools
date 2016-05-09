49848a2e4a15023a3dd4eb4a0ce83733be1c525f
fuxin_home <goitt0519@gmail.com>
2016-05-09 22:53:04 +0800

    add in test for dict to repleace for has_key

---
+++ example/dictkeys.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.6
+#-*- coding: utf-8 -*-
+
+d = {'a': 1, 'b': 2}
+print d
+if 'a' in d:
+    print d
+else:
+    print '123'
+
+L = ['2', '3', '4']
+if '3' in L:
+    print 'ok'
+else:
+    print '3 not in L'
