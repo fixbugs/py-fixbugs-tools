9180cdaddb603865d9d85a5990fc7d9b2bb9262b
fuxin_home <goitt0519@gmail.com>
2016-07-23 22:57:26 +0800

    add daemon class test

--- main.py
+++ main.py
@@ -88,6 +88,26 @@ def testThreadQueue():
             break
     print 'ok'
 
+
+def testDaemon():
+    imort sys
+    from de import Daemon
+    daemon = Daemon('/tmp/watch_process.pid', stdout='/tmp/watch_stdout.log')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print 'unknown command'
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print 'usage: %s start|stop|restart' % sys.argv[0]
+        sys.exit(2)
+
 if __name__ == '__main__':
     start_time = time.time()
     req = testThreadQueue()
