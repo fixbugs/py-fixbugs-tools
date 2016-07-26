91e5bdcb1969cbd8e90bdf4708886956d774eead
fuxin_home <goitt0519@gmail.com>
2016-07-26 22:04:05 +0800

    code rule

--- example/tranlate/tranlate.py
+++ example/tranlate/tranlate.py
@@ -59,32 +59,3 @@ if __name__ == '__main__':
     tcls = Tranlate(appid, secretKey)
     tcls.setQueryString('chinese english apple')
     print tcls.getResult()
-
-# httpClient = None
-# myurl = '/api/trans/vip/translate'
-# q = 'chinese apple'
-# fromLang = 'en'
-# toLang = 'zh'
-# salt = random.randint(32768, 65536)
-
-# sign = appid+q+str(salt)+secretKey
-# m1 = md5.new()
-# m1.update(sign)
-# sign = m1.hexdigest()
-# myurl = myurl+'?appid='+appid+'&q='+urllib.quote(q)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
-
-# try:
-#     httpClient = httplib.HTTPConnection('api.fanyi.baidu.com')
-#     httpClient.request('GET', myurl)
-
-#     #response是HTTPResponse对象
-#     response = httpClient.getresponse()
-#     res = response.read()
-#     ret = simplejson.loads(res)
-#     print ret
-#     print ret['trans_result'][0]['dst']
-# except Exception, e:
-#     print e
-# finally:
-#     if httpClient:
-#         httpClient.close()
