58b891d6fbe28714ad5e4ebb547f9429d570af9b
fuxin_home <goitt0519@gmail.com>
2016-07-29 22:31:15 +0800

    add code count without explains

--- codeline.py
+++ codeline.py
@@ -5,6 +5,7 @@ basedir = '/html/NginxServer/py-fixbugs-tools'
 filelists = []
 # 指定想要统计的文件类型
 whitelist = ['php', 'py']
+explainSkipList = ['#']
 
 
 #遍历文件, 递归遍历文件夹中的所有
@@ -26,12 +27,21 @@ def getFile(basedir):
 #统计一个文件的行数
 def countLine(fname):
     count = 0
+    with_out_explains = 0
     for file_line in open(fname).xreadlines():
         if file_line != '' and file_line != '\n':
             #过滤掉空行
             count += 1
+            file_line = file_line.lstrip()
+            if file_line[0] in explainSkipList:
+                continue
+            with_out_explains += 1
     print fname + '----', count
-    return count
+    print with_out_explains
+    result = dict()
+    result['count'] = count
+    result['with_out_explains'] = with_out_explains
+    return result
 
 
 def main():
@@ -39,9 +49,13 @@ def main():
     startTime = time.clock()
     getFile(basedir)
     totalline = 0
+    total_line_without_explains = 0
     for filelist in filelists:
-        totalline = totalline + countLine(filelist)
+        count_res = countLine(filelist)
+        totalline = totalline + count_res['count']
+        total_line_without_explains = total_line_without_explains + count_res['with_out_explains']
         print 'total lines:', totalline
+        print 'total lines with out explains', total_line_without_explains
         print 'Done! Cost Time: %0.2f second' % (time.clock() - startTime)
 
 if __name__ == '__main__':
