34003628920e32fd4ce72672a3cc2b16856a2a3b
fuxin_work_vir <goitt0519@gmail.com>
2016-08-10 02:14:31 -0400

    add test

--- test/test.py
+++ test/test.py
@@ -50,6 +50,23 @@ def box(data, max_num):
     result['box_link'] = '-'.join(map(str, box_arr))
     return result
 
-hd_arr = [509, 838, 924, 650, 604, 793, 564, 651, 697, 649, 747, 787, 701, 605, 644]
-max_num = 5000
-print box(hd_arr, max_num)
+# hd_arr = [509, 838, 924, 650, 604, 793, 564, 651, 697, 649, 747, 787, 701, 605, 644]
+# max_num = 5000
+# print box(hd_arr, max_num)
+
+
+def get_pass(pass_md5):
+    pass_md5 = pass_md5.lower()
+    import hashlib
+    pass_str = ''
+    for i in range(1990,2016):
+        for j in range(1,12):
+            for k in range(1,31):
+                y = str(i)
+                m = '0'+str(j) if j<10 else str(j)
+                d = '0'+str(k) if k<10 else str(k)
+                tmp_str = y+m+d
+                if hashlib.md5(tmp_str).hexdigest() == pass_md5:
+                    return tmp_str
+
+print get_pass('7E38890B870934B126F66857ED6B57B9')
