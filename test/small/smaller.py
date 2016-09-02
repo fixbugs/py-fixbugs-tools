7f0e2eacd9809269ec9d4bc3cd9df3df10d15b9d
fuxin_work_vir <goitt0519@gmail.com>
2016-09-02 14:01:41 +0800

    smaller and smaller slove

---
+++ test/small/smaller.py
@@ -0,0 +1,29 @@
+def wheelerTransform(data):
+    data = "^" + data + "|"
+    length = len(data)
+    rotations = []
+    for i in range(length, 0, -1):
+        rotations.append(data[i:length] + data[0:i])
+    rotations.sort()
+    result = ""
+    for r in rotations:
+        result += r[length - 1]
+    return result
+
+def compress(data):
+    result = ""
+    count = 1
+    cur = data[0]
+    for d in data[1:]:
+        if(d != cur):
+            result += cur + str(count)
+            count = 1
+            cur = d
+        else:
+            count += 1
+    return result + cur + str(count)
+
+if __name__=='__main__':
+    wt = wheelerTransform("BABABABANANABABABABABANANABABABABABANANABANANANAAHH")
+    print wt
+    print compress(wt)
