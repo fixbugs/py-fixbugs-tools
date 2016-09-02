7dd61ed0b5fa49fd7b8bea870c78d585ab4f17a7
fuxin_work_vir <goitt0519@gmail.com>
2016-09-02 17:08:27 +0800

    add smallest slove code

---
+++ test/small/smaller_more.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import hashlib
+
+def string_md5(c_str):
+    return hashlib.md5(c_str).hexdigest()
+
+def wheelerTransform(data):
+    data = "^" + data + "|"
+    length = len(data)
+    rotations = []
+    for i in range(length, 0, -1):
+        rotations.append(data[i:length] + data[0:i])
+    rotations.sort()
+    result = ""
+    for r in rotations:
+        result += r[length - 1]
+    return result
+
+def getFileContent(file_path):
+    import os
+    if not os.path.exists(file_path):
+        return False
+    result = []
+    try:
+        f = open(file_path, 'r')
+        result = f.readlines()
+    finally:
+        f.close()
+    return result
+
+def compress(data):
+    result = ""
+    count = 1
+    cur = data[0]
+    for d in data[1:]:
+        if(d != cur):
+            result += cur + str(count)
+            count = 1
+            cur = d
+        else:
+            count += 1
+    return result + cur + str(count)
+
+def bwt(string , slice_len):
+    string_len = len(string)
+    string_slice = []
+    for index in range(len(string)):
+        s , e = index , (index+slice_len)%string_len
+        if s < e :
+            string_slice.append((string[s:e] , string[s-1]))
+        else :
+            string_slice.append((string[s:] + string[:e] , string[s-1]))
+    return string_slice
+
+if __name__=='__main__':
+    sstr = getFileContent('war_and_piece.txt')
+    #wt = wheelerTransform("BABABABANANABABABABABANANABABABABABANANABANANANAAHH")
+    print len(sstr[0])
+    print bwt(sstr[0], 100)
+    exit(0)
+    wt = wheelerTransform(sstr[0])
+    print wt
+    print string_md5(wt)
