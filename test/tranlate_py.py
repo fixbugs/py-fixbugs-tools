4ded2af7ab1f1fd4ccf0df170f62381da51873c4
goitt <goitt0519@gmail.com>
2016-09-03 16:06:11 +0800

    add pinyin for search first slove

--- test/tranlate_py.py
+++ test/tranlate_py.py
@@ -7,7 +7,7 @@ import os
 
 def getStringPinYin(chineseString):
     chineseString = unicode(chineseString, 'utf-8')
-    print chineseString, lazy_pinyin(chineseString)
+    #print chineseString, lazy_pinyin(chineseString)
     pinYinString = ''.join(lazy_pinyin(chineseString))
     return pinYinString
 
@@ -39,18 +39,38 @@ def main():
         tmpDict = dict()
         tmpDict['word'] = word
         tmpPinYinResult = getStringPinYin(word)
-        if word == '银行发行股票':
-            tmpPinYinResult = 'yinhangfaxinggupiao'
-        elif word == '巴西汽车市场衰退通用或将重新考虑新投资':
-            tmpPinYinResult = 'baxiqicheshichangshuaituitongyonghuojiangchongxinkaolvxintouzi'
-        elif word == '大虾被称分离器':
-            tmpPinYinResult = 'daxiabeichenfenliqi'
-        elif word == '小沈阳退出喜剧人':
-            tmpPinYinResult = 'xiaoshenyangtuichuxijuren'
+        #卡戴珊姐妹曝隐私
+        #我们俩
+        #美人鱼曝纪录片
+        if '传' in word:
+            if word == '射雕英雄传之东成西就':
+                tmpPinYinResult = 'shediaoyingxiongzhuanzhidongchengxijiu'
+            elif '正传' in word:
+                tmpPinYinResult.replace('zhengchuan', 'zhengzhuan')
+            elif '莫扎特传' in word:
+                tmpPinYinResult = 'mozhatezhuan'
+            print word, tmpPinYinResult
+        if '曝' in word:
+            #pu 3+
+            tmpPinYinResult = tmpPinYinResult.replace('pu', 'bao')
+        if '沈' in word:
+            tmpPinYinResult = tmpPinYinResult.replace('chen', 'shen')
+        if '银行' in word:
+            #3+
+            tmpPinYinResult = tmpPinYinResult.replace('yinxing', 'yinhang')
+        if '重新' in word:
+            tmpPinYinResult = tmpPinYinResult.replace('zhongxin', 'chongxin')
+        if word == '报告称城镇学校教师职业倦怠比乡村教师更严重':
+            tmpPinYinResult = 'baogaochenchengzhenxuexiaojiaoshizhiyejuandaibixiangcunjiaoshigengyanzhong'
+        #elif word == '小沈阳退出喜剧人':
+        #    print tmpPinYinResult, word
+        #    tmpPinYinResult = 'xiaoshenyangtuichuxijuren'
+        #elif word == '中国光大银行九江分行盛大开业':
+        #    tmpPinYinResult = 'zhongguoguangdayinhangjiujiangfenhangshengdakaiye'
         tmpDict['pinyin'] = tmpPinYinResult
         tranlateResult.append(tmpDict)
         pinyinList.append(tmpPinYinResult)
-    #print '-'.join(pinyinList)
+    print '-'.join(pinyinList)
     return tranlateResult
 
 if __name__ == '__main__':
