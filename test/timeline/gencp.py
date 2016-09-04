351f63cfda769b7f0e2d6bad3f6abf72441c4bae
goitt <goitt0519@gmail.com>
2016-09-04 22:51:11 +0800

    fix timeline slove element

--- test/timeline/gencp.py
+++ test/timeline/gencp.py
@@ -76,7 +76,7 @@ for i in xrange(limit):
                 if tmp_d['person'] == x['person']:
                     continue
                 if tmp_d['person'] % x['person'] == 0:
-                    print 'content check', i
+                    #print 'content check', i
                     speak_str.append(x['content'])
         if len(speak_str) > 0:
             v_result.append(string_md5('-'.join(speak_str)))
