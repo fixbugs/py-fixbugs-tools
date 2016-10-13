f62cf5c7a59cc392a31cdfbe68a8df1395ac0638
goitt <goitt0519@gmail.com>
2016-10-13 21:41:43 +0800

    remove debug code

--- test/modu/ttt.php
+++ test/modu/ttt.php
@@ -80,14 +80,6 @@ if(isset($argv[1]) && isset($argv[2]) ){
 
 endFourPiecesString();
 
-// if($argv[1] && $argv[2]){
-//     $startX = $argv[1];
-//     $startY = $argv[2];
-// }else{
-//     $startX = -1;
-//     $startY = -1;
-// }
-
 if($is_rsort){
     calRsort($piece_array,0,$map);
 }else{
