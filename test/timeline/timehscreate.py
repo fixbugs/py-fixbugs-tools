50869cb4c1eeef6ee3fab16e9ea8e3d9c1332bf0
goitt <goitt0519@gmail.com>
2016-10-25 21:34:20 +0800

    add timeline slove for 1

--- test/timeline/timehscreate.py
+++ test/timeline/timehscreate.py
@@ -8,24 +8,24 @@ import numpy
 import string
 
 
-def getFileContent(file_path):
-    import os
-    if not os.path.exists(file_path):
-        return False
-    result = []
-    try:
-        f = open(file_path, 'r')
-        result = f.readlines()
-    finally:
-        f.close()
-    return result
+# def getFileContent(file_path):
+#     import os
+#     if not os.path.exists(file_path):
+#         return False
+#     result = []
+#     try:
+#         f = open(file_path, 'r')
+#         result = f.readlines()
+#     finally:
+#         f.close()
+#     return result
 
-cont = getFileContent('time_line_res.txt')
-print len(cont)
-for t in cont:
-    tm = t[1:]
-    print hashlib.md5(tm).hexdigest()
-exit(0)
+# cont = getFileContent('time_line_res.txt')
+# print len(cont)
+# for t in cont:
+#     tm = t[1:]
+#     print hashlib.md5(tm).hexdigest()
+# exit(0)
 
 global_digest_list = list()
 if sys.version_info[0] == 3:
