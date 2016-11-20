e1fcebcea4f9baa590fa7294793902d5adbca00f
goitt <goitt0519@gmail.com>
2016-11-20 20:35:21 +0800

    add kaisa slove

--- test/haffman/75d2-kaisa.py
+++ test/haffman/75d2-kaisa.py
@@ -7,6 +7,7 @@ print len(secretStr)
 
 
 def repleaceStr(oldStr, n):
+    s = oldStr
     if ord(s) >= 65 and ord(s) <= 90:
         #print oldStr,ord(oldStr)+n
         if ord(oldStr) + n > 90:
@@ -26,6 +27,24 @@ def repleaceStr(oldStr, n):
         newStr = oldStr
     return newStr
 
+
+def crack_transposition(cipher, msg_col_dim):
+    msg_ch_arr = ['-' for n in range(len(cipher))]
+    msg_row_dim = (len(cipher) + (msg_col_dim - 1)) / msg_col_dim
+    idx_step = msg_row_dim - 1
+    start_change_idx = (len(cipher) % msg_col_dim + 1) * msg_row_dim - 1
+
+    for idx in range(0, len(cipher)):
+        final_index = idx
+        if final_index >= start_change_idx:
+            final_index += (final_index - start_change_idx + idx_step) / idx_step
+        msg_row_idx = final_index % msg_row_dim
+        msg_col_idx = final_index / msg_row_dim
+        msg_idx = msg_row_idx * msg_col_dim + msg_col_idx
+        msg_ch_arr[msg_idx] = cipher[idx]
+
+    return ''.join(msg_ch_arr)
+
 #n= -6 k=>e
 #print ord('a'), ord('k')
 #exit(0)
@@ -37,7 +56,11 @@ for s in secretStr:
     else:
         countStrDict[s] = 1
 
+
 print countStrDict
+print sorted(countStrDict.items(), key=lambda item: item[1])
+#k=> e
+#print countStrDict
 
 
 def getUnSecStr(secStr, n):
@@ -102,17 +125,25 @@ def deZLStr(zlstr, n):
 
 # for i in range(-26, 26):
 #     unsecStr = getUnSecStr(secretStr, i)
+#print crack_transposition(secretStr, 4)
+#exit(0)
 
-unsecStr = secretStr
-print len(secretStr)
+unsecStr = getUnSecStr(secretStr, -20)
+unsecStr = getUnSecStr(secretStr, -6)
+print len(unsecStr)
+
+#unsecStr = secretStr
+#print len(secretStr)
 
 #test 50 is end '.'
-for j in range(1, 150):
-    deres = deZLStr(unsecStr, j)
+for j in range(1, 10):
+    #deres = deZLStr(secretStr, j)
+    deres = crack_transposition(unsecStr, j)
     #print unsecStr, len(deres)
     if deres.endswith('.'):
+        if deres.endswith(' .'):
+            continue
         print deres, j
-        exit(0)
 
 # for i in range(1, 27):
 #     deres = deZLStr(unsecretStr, i)
