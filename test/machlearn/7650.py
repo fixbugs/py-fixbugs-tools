3fd41bc04a824af09556aeb1a71f385f3f799690
goitt <goitt0519@gmail.com>
2016-11-25 21:31:08 +0800

    add 7650 svd test

--- test/machlearn/7650.py
+++ test/machlearn/7650.py
@@ -35,10 +35,38 @@ def getFileContent(file_path):
         f.close()
     return result[1:]
 
+uidArr = []
+movieArr = []
+
+
+def getTrainMaxi(trainarr):
+    #uidArr = []
+    totalArr = np.zeros((500, 11000))
+    totalArr = np.ndarray.tolist(totalArr)
+    #movieArr = []
+    for t in trainarr:
+        if t[0] not in uidArr:
+            uidArr.append(t[0])
+        if t[1] not in movieArr:
+            movieArr.append(t[1])
+        Uindex = int(uidArr.index(t[0]))
+        Mindex = int(movieArr.index(t[1]))
+        totalArr[Uindex][Mindex] = int(t[2])
+    return totalArr
+
+
+# trainarr = []
+# tmp = [1, 2, 5]
+# trainarr.append(tmp)
+# trainarr.append([1,3,1])
+# trainarr.append([2,2,3])
+# trainarr.append([2,3,1])
 trainarr = getFileContent('7650d/train.txt')
-testarr = getFileContent('7650d/test.txt')
+trainres = getTrainMaxi(trainarr)
+print trainres
+#testarr = getFileContent('7650d/test.txt')
 # first get train maxirt for reuslt
 # piece for all result
-U, s, Vh = linalg.svd(trainarr)
+U, s, Vh = linalg.svd(trainres)
 print s
 #print testarr
