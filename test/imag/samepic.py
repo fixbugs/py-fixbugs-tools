bfc64b2feaa868ef223cd66db3a00861f717d48f
fuxin_work_vir <goitt0519@gmail.com>
2016-12-01 21:06:14 +0800

    add more

--- test/imag/samepic.py
+++ test/imag/samepic.py
@@ -2,25 +2,48 @@
 #coding=utf8
 from PIL import Image
 import imagehash
+import os
 # hash = imagehash.average_hash(Image.open('lenna.png'))
 # print hash
 
 def getImhash(picpath):
     return imagehash.average_hash(Image.open(picpath))
 
-SP_PATH = 'image2\'
-DR_PATH = 'image1\'
+def GetFileList(dir, fileList):
+    newDir = dir
+    if os.path.isfile(dir):
+        fileList.append(dir.decode('gbk'))
+    elif os.path.isdir(dir):
+        for s in os.listdir(dir):
+            #如果需要忽略某些文件夹，使用以下代码
+            #if s == "xxx":
+                #continue
+            newDir=os.path.join(dir,s)
+            GetFileList(newDir, fileList)
+    return fileList
+
+SP_PATH = 'image2/'
+DR_PATH = 'image1/'
 
 sp_pic_hash_arr = []
 
-for i in range(1,31):
-    tmp_path = SP_PATH + str(i)
+sp_files = GetFileList(SP_PATH)
+
+#for i in range(1,31):
+for i in sp_files:
+    #tmp_path = SP_PATH + str(i)
+    tmp_path = i
     sp_pic_hash_arr.append(getImhash(tmp_path))
 
+dr_files = GetFileList(DR_PATH)
 dr_pic_hash_arr = []
 total = 0
-for j in range(1,5001):
-    tt_path = DR_PATH + str(i)
+count = 0
+#for j in range(1,5693):
+for j in dr_files:
+    count += 1
+    #tt_path = DR_PATH + str(i)
+    tt_path = j
     now_hash = getImhash(tt_path)
     if now_hash in sp_pic_hash_arr:
         total += j
