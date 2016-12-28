e216dce8b3d9474862c49b891a419fb8a709d0b2
goitt <goitt0519@gmail.com>
2016-12-28 23:01:28 +0800

    test 75e4

--- test/75e4/75e4.py
+++ test/75e4/75e4.py
@@ -26,6 +26,46 @@ class ball(object):
         self._nowSpX = newX
         self._nowSpY = newY
 
-    def getNextChangeTime():
+    def getNextChangeTime(self):
+        xtime = 0
+        ytime = 0
+        if self._nowSpX > 0:
+            if self._nowX < MAXX/2:
+                #right
+                xtime = float((MAXX - self._nowX)/self._nowSpX)
+            else:
+                #left
+                xtime = float((self._nowX)/self._nowSpX)
+        else:
+            if self._nowX < MAXX/2:
+                #right
+                xtime = abs(float((self._nowX)/self._nowSpX))
+            else:
+                #left
+                xtime = abs(float((MAXX - self._nowX)/self._nowSpX))
+        if self._nowSpY > 0:
+            if self._nowY < MAXY/2:
+                ytime = float((MAXY - self._nowY)/self._nowSpY)
+            else:
+                ytime = float((self._nowY)/self._nowSpY)
+        else:
+            if self._nowY < MAXY/2:
+                ytime = abs(float((self._nowY)/self._nowSpY))
+            else:
+                ytime = abs(float((MAXY - self._nowY)/self._nowSpY))
+        print xtime,ytime
         #get next change time
-        pass
+        return min(xtime, ytime)
+
+tb = ball(10,10, 1,2)
+nt1 = tb.getNextChangeTime()
+print tb.countEnd(nt1)
+print tb._nowX
+print tb._nowY
+
+
+print "============="
+
+tb2 = ball(80,80, -15,-16)
+print tb2.getNextChangeTime()
+
