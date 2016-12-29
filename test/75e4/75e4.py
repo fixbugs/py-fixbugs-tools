22639e4f1962854ae2cd5f598f7a4150655b1a87
goitt <goitt0519@gmail.com>
2016-12-29 22:28:03 +0800

    add ball speed count

--- test/75e4/75e4.py
+++ test/75e4/75e4.py
@@ -3,6 +3,7 @@
 
 
 import numpy as np
+import math
 
 
 MAXX = 1000
@@ -17,6 +18,8 @@ class ball(object):
         self._nowY = initY
         self._nowSpX = spX
         self._nowSpY = spY
+        self._spSQ = 0
+        self.countSpeedSQ()
 
     def countEnd(self, timePass):
         self._nowX += timePass * self._nowSpX
@@ -25,6 +28,12 @@ class ball(object):
     def setNewSpeed(self, newX, newY):
         self._nowSpX = newX
         self._nowSpY = newY
+        self.countSpeedSQ()
+
+    def countSpeedSQ(self):
+        self._spSQ = math.sqrt(pow(abs(self._nowSpX), 2) + pow(abs(self._nowSpY), 2))
+        if self._nowSpX < 0 and self._nowSpY >0 or self._nowSpX >0 and self._nowSpY < 0:
+            self._spSQ = -1 * self._spSQ
 
     def getNextChangeTime(self):
         xtime = 0
@@ -62,10 +71,11 @@ nt1 = tb.getNextChangeTime()
 print tb.countEnd(nt1)
 print tb._nowX
 print tb._nowY
-
+print tb._spSQ
 
 print "============="
 
 tb2 = ball(80,80, -15,-16)
 print tb2.getNextChangeTime()
+print tb2._spSQ
 
