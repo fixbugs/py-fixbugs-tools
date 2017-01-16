2a65e2f992d1c9b0dbe4dc9bde8384fd362ecadf
fuxin_work_vir <goitt0519@gmail.com>
2017-01-16 17:26:33 +0800

    excel sheet to tile slove code

--- leetcode/168excelsheet.py
+++ leetcode/168excelsheet.py
@@ -9,37 +9,42 @@ class Solution(object):
         :type n: int
         :rtype: str
         """
-        result = list()
-    #     for i in range(0,n):
-    #         result.append(self.getNumStr(i))
-    #     return result
-
-    # def getNumStr(self, n):
+        num = n
+        return "" if num == 0 else self.convertToTitle((num - 1) / 26) + chr((num - 1) % 26 + ord('A'))
+        print chr(64+1)
         numArr = self.numDoublesToList(n, 26)
         print numArr
         resultStr = ''
         for num in numArr:
-            resultStr = resultStr + chr(64+num)
+            if num > 0:
+                resultStr = resultStr + chr(64+num)
         return resultStr
 
     def numDoublesToList(self, num, basen):
-        if num < basen:
-            return [num]
-        else:
-            dnum = num/basen
-            nnum = num
-            result = list()
-            while nnum/basen:
-                result.append(int(nnum/basen))
-                nnum = nnum - int(nnum/basen) * basen
-            tnum = num - dnum*basen
-            #result = list()
-            #result.append(dnum)
-            result.extend(self.numDoublesToList(tnum, basen))
-            return result
+        result = list()
+        while True:
+            num, remainder = divmod(num, basen+1)
+            result.append(remainder)
+            if num == 0:
+                return result[::-1]
+        #return result
+        # if num < basen:
+        #     return [num]
+        # else:
+        #     dnum = num/basen
+        #     nnum = num
+        #     result = list()
+        #     while nnum/basen:
+        #         result.append(int(nnum/basen))
+        #         nnum = nnum - int(nnum/basen) * basen
+        #     tnum = num - dnum*basen
+        #     #result = list()
+        #     #result.append(dnum)
+        #     result.extend(self.numDoublesToList(tnum, basen))
+        #     return result
 
 
 if __name__ == "__main__":
     a = Solution()
-    n = 24568
+    n = 26
     print a.convertToTitle(n)
