f9c3474ea556557db3d092fab17e0c9244c8c6b8
fuxin_work_vir <goitt0519@gmail.com>
2017-01-17 16:30:18 +0800

    add longest sub string get

---
+++ leetcode/3longestsubstring.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf8
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+if __name__ == "__main__":
+    a = Solution()
+    s = "abcabcbb"
+    print a.lengthOfLongestSubstring(s)
