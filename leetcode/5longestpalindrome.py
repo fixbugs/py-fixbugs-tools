148ce454161f20ade367e1bf431de6108413aa90
goitt <goitt0519@gmail.com>
2017-01-22 21:21:42 +0800

    init longest palind rome

---
+++ leetcode/5longestpalindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf8
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        print s
+        pass
+
+if __name__ == "__main__":
+    a = Solution()
+    n = 'cbbd'
+    print a.longestPalindrome(n)
