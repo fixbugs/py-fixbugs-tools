c46e0817f8129b7d443b9cc9c7378d8ae4921b07
goitt <goitt0519@gmail.com>
2017-01-24 23:05:18 +0800

    add zigzag conver test func

---
+++ leetcode/6zigzagconver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#coding=utf8
+
+
+class Solution(object):
+    def test(self):
+        pass
+
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        pass
+
+
+if __name__ == "__main__":
+    a = Solution()
+    s = 'njkjkjnjff'
+    n = 3
+    print a.convert(s, n)
