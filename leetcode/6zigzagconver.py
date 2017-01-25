8a971b8ea9b8d35e7bfa767dd4f72eda20c7b188
goitt <goitt0519@gmail.com>
2017-01-25 22:34:13 +0800

    add zigzag test

--- leetcode/6zigzagconver.py
+++ leetcode/6zigzagconver.py
@@ -13,6 +13,21 @@ class Solution(object):
         :rtype: str
         """
         pass
+	if numRows == 1 or numRows >= len(s):
+            return s
+
+        L = [''] * numRows
+        index, step = 0, 1
+
+        for x in s:
+            L[index] += x
+            if index == 0:
+                step = 1
+            elif index == numRows -1:
+                step = -1
+            index += step
+
+        return ''.join(L)
 
 
 if __name__ == "__main__":
